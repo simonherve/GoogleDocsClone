2c286a8cfd8e709d119ed3febd5208d6e6ada53d
Simon HERVE <simon.joseph.herve@gmail.com>
2025-10-04 23:40:44 +0200

    add link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
-import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 
 const Home = () => {
   return (
     <div className="flex min-h-screen items-center justify-center">
-      <Button>
-        Click me
-      </Button>
+      Click <Link href="/documents/123">
+        <span className="text-blue-500 underline">&nbsp;here&nbsp;</span>
+      </Link> to go to document id
     </div>
   );
 }
